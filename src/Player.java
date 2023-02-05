700d1df518ce9f92b17597219a83fdbc61250d0d
Dzhaparov Bekmamat <122871322+DzhaparovBekmamat@users.noreply.github.com>
2023-02-06 00:21:11 +0600

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,32 +1,28 @@
 public class Player {
         private int health;
         private final String name;
-
         public Player(int health, String name) {
             this.health = health;
             this.name = name;
         }
-
         public int getHealth() {
             return this.health;
         }
-
         public void setHealth(int health) {
             this.health = health;
         }
-
         public String getName() {
             return this.name;
         }
+         public void setName(int health) {
+            this.name = name;
     }
-
     class Medic extends Player {
         private final int healingPower;
         public Medic(int health, String name, int healingPower) {
             super(health, name);
             this.healingPower = healingPower;
         }
-
         public void heal(Player player) {
             if (player.getHealth() < 100 && this.getHealth() > 0 && player != this) {
                 player.setHealth(player.getHealth() + this.healingPower);
@@ -44,12 +40,10 @@ public class Player {
             this.setHealth(this.getHealth() - reducedDamage);
         }
     }
-
     class Thor extends Player {
         public Thor(int health, String name) {
             super(health, name);
         }
-
         public boolean stun() {
             double chance = Math.random();
             if (chance <= 0.3) {
@@ -59,23 +53,19 @@ public class Player {
             return false;
         }
     }
-
     class Team {
         private Player[] players;
         private Medic medic;
-
         public Team(Player[] players, Medic medic) {
             this.players = players;
             this.medic = medic;
         }
-
         public void takeDamage(int damage) {
             for (Player player : players) {
                 player.setHealth(player.getHealth() - damage);
             }
             medic.takeDamage();
         }
-
         public void heal() {
             for (Player player : players) {
                 if (player.getHealth() < 100) {
@@ -84,7 +74,6 @@ public class Player {
                 }
             }
         }
-
         public boolean stun() {
             for (Player player : players) {
                 if (player instanceof Thor) {
@@ -93,7 +82,6 @@ public class Player {
             }
             return false;
         }
-
         public void printTeamStatus() {
             for (Player player : players) {
                 System.out.println(player.getName() + ": " + player.getHealth() + " health");
